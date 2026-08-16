726b0d19a10e58121078513f96c6cacb49c05121
PET-3 <yyx3307022@gmail.com>
2026-08-16 18:17:48 +0800

    Fix import statement for Modifier in HomeScreen.kt

--- app/src/main/java/com/example/webmirror/ui/HomeScreen.kt
+++ app/src/main/java/com/example/webmirror/ui/HomeScreen.kt
@@ -52,7 +52,7 @@ import android.content.Intent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
