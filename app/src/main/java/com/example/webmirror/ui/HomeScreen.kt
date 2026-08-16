2e78c42928df4abd073e40155502045f670e88ac
PET-3 <yyx3307022@gmail.com>
2026-08-16 15:27:26 +0800

    Update HomeScreen.kt

--- app/src/main/java/com/example/webmirror/ui/HomeScreen.kt
+++ app/src/main/java/com/example/webmirror/ui/HomeScreen.kt
@@ -48,7 +48,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
